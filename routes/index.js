f4465ac2194b39718fddc9697704df169796d6c9
Sovann20 <sovannchak20@live.com>
2017-04-03 14:08:38 -0500

    Removed Inline Styles & Switched to Bootstrap styles

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,10 @@ router.get('/signup', function (req, res, next) {
     res.render('signup', {title: 'Sign up'});
 });
 
+router.get('/home', function (req, res, next) {
+   res.render('home');
+});
+
 router.post('/user_signup', function (req, res, next) {
     var user = {
         username: req.body.username,
@@ -27,14 +31,32 @@ router.post('/user_signup', function (req, res, next) {
 
     mongo.connect(url, function (err, db) {
         assert.equal(null, err);
-        db.collection('user-data').insertOne(user, function () {
-            assert.equal(null, err);
-            console.log("User inserted");
-            db.close();
+
+        var isValue = 1;
+        db.collection('user-data').count({ 'user.username': user.username },
+            function (err, count) {
+            console.log("Count: " +count);
+            // console.log("Username: "+ username);
+            console.log("User.username: "+user.username);
         });
+        // console.log("start: "+temp+" end");
+
+        // var isValue = 1;
+
+        if(isValue == 0) {
+            db.collection('user-data').insertOne(user, function () {
+                assert.equal(null, err);
+                console.log("User inserted");
+                db.close();
+                res.redirect('/login');
+            });
+        } else {
+            console.log("Username already exists!");
+            res.render('signup');
+        }
     });
 
-    res.redirect('/login');
+
 });
 
 
@@ -45,7 +67,7 @@ router.get('/userLogin', function (req, res, next) {
 
     mongo.connect(url, function (err, db) {
         assert.equal(null, err);
-        var cursor = db.collection('user-data').find();
+        var cursor = db.collection('user-data').find({username : 'Sovann'});
         cursor.forEach(function (doc, err) {
             assert.equal(null, err);
             resultArr.push(doc);
